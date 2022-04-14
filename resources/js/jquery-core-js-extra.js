663b58ca757a76d64a71ca640c0fb5e36c2b4101
sastelvio <41056057+sastelvio@users.noreply.github.com>
2022-04-14 18:07:35 +0200

    Add files via upload

---
+++ resources/js/jquery-core-js-extra.js
@@ -0,0 +1,4 @@
+/* <![CDATA[ */
+var load_more_demo_ajax = {"ajax_url": "https:\/\/demo.cmssuperheroes.com\/themeforest\/facmaster\/wp-admin\/admin-ajax.php", "pre_img": "https:\/\/demo.cmssuperheroes.com\/demo-bar\/"};
+var swadmb_current_theme = {"current_theme": "wp-theme-facmaster"};
+/* ]]> */
